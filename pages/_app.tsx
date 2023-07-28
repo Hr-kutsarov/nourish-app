b655e7eac8c8b83f6873a448a5be1cd76f322370
Hr-kutsarov <hr.kutsarov89@gmail.com>
2023-07-28 18:47:31 +0300

    Added react-query provider and AuthModal.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,27 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { SessionProvider } from "next-auth/react"
 
+import { useState } from 'react'
+
 import Header from '@/components/header/Header'
+import AuthModal from '@/components/modals/auth-modal'
 
 export default function App({ Component, pageProps: { session, ...pageProps} }: AppProps) {
+  const [client] = useState(() => new QueryClient())
+
   return (
-    <SessionProvider session={session}>
-      <Header />
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryClientProvider client={client}>
+      <SessionProvider session={session}>
+        <Header />
+        <AuthModal />
+        <Component {...pageProps} />
+        <ReactQueryDevtools />
+      </SessionProvider>
+    </QueryClientProvider>
+
   )
 }
