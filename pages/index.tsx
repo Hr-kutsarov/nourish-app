33deaafb4ff12707e7dc0e4dd6c0f03fe3f28cf5
Hr-kutsarov <hr.kutsarov89@gmail.com>
2023-07-28 18:47:57 +0300

    Home page cleanup.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,29 +3,15 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { useSession, signIn, signOut } from "next-auth/react"
 
-export default function Home({}) {
-  const { data: session }:any = useSession();
+export default function Home() {
 
-  if (session) {
-    return (
-      <div className='flex flex-col gap-4 px-12 py-8 rounded-lg'>
-        <h1>Signed in as {session?.user.email}</h1>
-        <Link href='/profile'>Navigate to Profile</Link>
-        <button onClick={() => signOut()}>Sign Out</button>
-      </div>
-    )
-  }
 
   return (
     <main
-      className={`flex min-h-screen flex-col items-center gap-4 p-24 ${inter.className}`}
+      className={`flex w-full flex-col items-center justify-center gap-4 p-24 ${inter.className}`}
     >
-      <button className='bg-green-600 px-5 py-3 rounded-lg' onClick={() => signIn('github', {redirect: true})}>Sign in with GitHub</button>
-      <button className='bg-blue-400 px-5 py-3 rounded-lg' onClick={() => signIn('google')}>Sign in with Google</button>
-      <Link href='/profile'>Navigate to Profile</Link>
+      <h1>Home Page</h1>
     </main>
   )
 }
